9680e74b432c0ce4813868505bb5e01bea5fb368
Salam <salam73@gmail.com>
2023-02-03 22:05:35 +0300

    Initial commit

---
+++ lib/screens/main_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:school/screens/steps.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mainpage'),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
+            InkWell(
+              onTap: (){
+                Navigator.push(
+                  context,MaterialPageRoute(builder: (context)=>const Steps())
+                );
+              },
+              child: const Card(
+                child: SizedBox(
+                  width: 300,
+                  height: 60,
+                  child: Center(
+                    child: Text('هندسة تنقنيات الحاسوب'),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 100,),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('بـــــحوث التـــــخرج'),
+                    ),
+                  ),
+                ),  Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('كتـــــب عامـــــــــة'),
+                    ),
+                  ),
+                ),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/steps.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Steps extends StatelessWidget {
+  const Steps({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Steps'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+          children: const [
+            Card(
+              child: SizedBox(
+                width: 300,
+                height: 60,
+                child: Center(
+                  child: Text('المرحلة الاولى'),
+                ),
+              ),
+            ),
+            Card(
+              child: SizedBox(
+                width: 300,
+                height: 60,
+                child: Center(
+                  child: Text('المرحلة الثاني'),
+                ),
+              ),
+            ),
+            Card(
+              child: SizedBox(
+                width: 300,
+                height: 60,
+                child: Center(
+                  child: Text('المرحلة الثالث'),
+                ),
+              ),
+            ),
+            Card(
+              child: SizedBox(
+                width: 300,
+                height: 60,
+                child: Center(
+                  child: Text('المرحلة الرابعة'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
