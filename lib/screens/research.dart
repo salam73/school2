334771f9ba5bc16fa378a97a6160ae61ab893456
Salam <salam73@gmail.com>
2023-02-03 22:36:10 +0300

    update flutter

--- lib/screens/research.dart
+++ lib/screens/research.dart
@@ -7,10 +7,96 @@ class Research extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('research'),),
-      body: Column(
-        children: [
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-        ],
+              children: const [
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('الصيدلة'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('طب أسنان'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('قانون'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('إدارة أعمال'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+              children: const [
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('هندسة حاسبات'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('تحليلات مرضية'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('رياضة'),
+                    ),
+                  ),
+                ),
+                Card(
+                  child: SizedBox(
+                    width: 150,
+                    height: 60,
+                    child: Center(
+                      child: Text('تمريض'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
